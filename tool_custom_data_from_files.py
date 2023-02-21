5eff96402df3df95dc201b32f83e8e87040f7d63
dinglezhang <polycplusplus@gmail.com>
2023-02-21 16:13:23 +0800

    refactor tool_custom_data_from_files.py to transform data one by one

--- tool_custom_data_from_files.py
+++ tool_custom_data_from_files.py
@@ -25,16 +25,6 @@ def gen_custom_data_from_files(data_dir):
                       algorithm=None)
     sim.run(1)
 
-    # all data that need to tranform and write to one file
-    all_data_needed = [
-        sim.dmgr.ref_pos,
-        sim.dmgr.ref_att_euler,
-        sim.dmgr.ref_vel,
-        sim.dmgr.ref_accel,
-        sim.dmgr.ref_gyro,
-        sim.dmgr.ref_odo
-    ]
-
     description = \
 '''1~3: true LLA pos in the navigation frame
 4~6: true attitude (Euler angles, XYZ)
@@ -46,14 +36,16 @@ def gen_custom_data_from_files(data_dir):
     output_units = [
         'deg', 'deg', 'm',
         'rad', 'rad', 'rad',
+        #'deg', 'deg', 'deg',
         'm/s', 'm/s', 'm/s',
         'm/s^2', 'm/s^2', 'm/s^2',
         'rad/s', 'rad/s', 'rad/s',
+        #'deg/s', 'deg/s', 'deg/s',
         'm'
     ]
     legend = [
-        'pos_lat', 'pos_lon', 'pos_alt',
-        'Pitch', 'Roll', 'Yaw',
+        'lon', 'lat', 'alt',
+        'pitch', 'roll', 'yaw',
         'vel_x', 'vel_y', 'vel_z',
         'accel_x', 'accel_y', 'accel_z',
         'gyro_x', 'gyro_y', 'gyro_z',
@@ -64,46 +56,60 @@ def gen_custom_data_from_files(data_dir):
                                     units=[],\
                                     output_units=output_units,\
                                     legend=legend)
+    custom_data.units = []  # set units back since it is set to output_units by Sim_data
 
+    # all data that need to tranform and write to one file
+    all_data_needed = [
+        sim.dmgr.ref_pos,
+        sim.dmgr.ref_att_euler,
+        sim.dmgr.ref_vel,
+        sim.dmgr.ref_accel,
+        sim.dmgr.ref_gyro,
+        sim.dmgr.ref_odo
+    ]
+    row_num = -1
     for data in all_data_needed:
         if data.name in sim.dmgr.available:
             custom_data.units.extend(data.units)
-            if (len(custom_data.data) == 0):  # first time to combine data
-                custom_data.data = data.data
+            cur_row_num = data.data.shape[0]
+            if (row_num == -1):
+                row_num = cur_row_num
             else:
-              if (data.name == 'ref_odo'):
-                  ref_odo_2d = np.atleast_2d(data.data)
-                  ref_odo_2d = np.transpose(ref_odo_2d)
-                  custom_data.data = np.append(custom_data.data, ref_odo_2d, 1)
-              else:
-                  custom_data.data = np.append(custom_data.data, data.data, 1)
+              if (row_num != cur_row_num):
+                raise ValueError('Row number of %s data is wrong'% data.name)
+        else:
+            raise ValueError('%s data is missing'% data.name)
 
-    # ref_Yaw, ref_Pitch, ref_Roll -> ref_Roll, ref_Yaw, ref_Pitch
-    tmp_column = custom_data.data[:, 3]
-    tmp_array = np.delete(custom_data.data, 3, 1)
-    custom_data.data = np.insert(tmp_array, 5, tmp_column, 1)
+    custom_data.data = np.zeros((row_num, 16))
+
+    # lat, lon, alt -> lon, lat, alt
+    custom_data.data[:, 0] = sim.dmgr.ref_pos.data[:, 1]
+    custom_data.data[:, 1] = sim.dmgr.ref_pos.data[:, 0]
+    custom_data.data[:, 2] = sim.dmgr.ref_pos.data[:, 2]
+
+    # ref_Yaw, ref_Pitch, ref_Roll -> pitch, roll, yaw
+    custom_data.data[:, 3] = sim.dmgr.ref_att_euler.data[:, 1]
+    custom_data.data[:, 4] = sim.dmgr.ref_att_euler.data[:, 2]
+    custom_data.data[:, 5] = sim.dmgr.ref_att_euler.data[:, 0]
 
     # ref_vel from NED to ENU
-    tmp_column = custom_data.data[:, 6]
-    tmp_array = np.delete(custom_data.data, 6, 1)
-    custom_data.data = np.insert(tmp_array, 7, tmp_column, 1)
-    custom_data.data[:, 8] = -custom_data.data[:, 8]
+    custom_data.data[:, 6] = sim.dmgr.ref_vel.data[:, 1]
+    custom_data.data[:, 7] = sim.dmgr.ref_vel.data[:, 0]
+    custom_data.data[:, 8] = -sim.dmgr.ref_vel.data[:, 2]
 
     # ref_accel from NED to ENU
-    tmp_column = custom_data.data[:, 9]
-    tmp_array = np.delete(custom_data.data, 9, 1)
-    custom_data.data = np.insert(tmp_array, 10, tmp_column, 1)
-    custom_data.data[:, 11] = -custom_data.data[:, 11]
+    custom_data.data[:, 9] = sim.dmgr.ref_accel.data[:, 1]
+    custom_data.data[:, 10] = sim.dmgr.ref_accel.data[:, 0]
+    custom_data.data[:, 11] = -sim.dmgr.ref_accel.data[:, 2]
 
     # ref_gyro from NED to ENU
-    tmp_column = custom_data.data[:, 12]
-    tmp_array = np.delete(custom_data.data, 12, 1)
-    custom_data.data = np.insert(tmp_array, 13, tmp_column, 1)
-    custom_data.data[:, 14] = -custom_data.data[:, 14]
+    custom_data.data[:, 12] = sim.dmgr.ref_gyro.data[:, 1]
+    custom_data.data[:, 13] = sim.dmgr.ref_gyro.data[:, 0]
+    custom_data.data[:, 14] = -sim.dmgr.ref_gyro.data[:, 2]
 
     # ref_odo to ref_dist
     custom_data.units[15] = 'm'
-    custom_data.data[:, 15] = custom_data.data[:, 15]/100
+    custom_data.data[:, 15] = sim.dmgr.ref_odo.data.reshape((row_num, 1))[:, 0]/fs
 
     # save custom date file and readme
     print('saving %s'% custom_data.name)
