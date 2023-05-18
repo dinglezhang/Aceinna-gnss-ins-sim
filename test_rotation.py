2271bafc99ca62c3dbe9b0159bade977e94323e3
dinglezhang <polycplusplus@gmail.com>
2023-05-18 17:40:14 +0800

    only keep tests with gnss_ins_sim.attitude and move rest of tests to EasyEuler repo

--- test_rotation.py
+++ test_rotation.py
@@ -110,35 +110,6 @@ def euler_2_x(euler_d_input, rot_seq):
     print()
 
 '''
-to rotate by:
-    euler angles once
-    euler angles 3 times one by one
-they should get the same results
-'''
-def euler_one_by_one(euler_d_input, rot_seq):
-    print('~~~~~~~~~~euler one by one~~~~~~~~~~')
-
-    euler_r_input = euler_d_input * attitude.D2R
-    print('euler input by %s sequence intrinsicly:'% rot_seq)
-    print('%s (%s)'% (euler_d_input, euler_r_input))
-
-    rot_once = Rotation.from_euler(rot_seq.upper(), euler_r_input)
-    euler_d_once = rot_once.as_euler(rot_seq.upper(), True)
-    euler_r_once = euler_d_once * attitude.D2R
-
-    rot1 = Rotation.from_euler(rot_seq.upper(), [euler_r_input[0], 0, 0])
-    rot2 = Rotation.from_euler(rot_seq.upper(), [0, euler_r_input[1], 0])
-    rot3 = Rotation.from_euler(rot_seq.upper(), [0, 0, euler_r_input[2]])
-    rot_one_by_one = rot1 * rot2 * rot3
-    euler_d_one_by_one = rot_one_by_one.as_euler(rot_seq.upper(), True)
-    euler_r_one_by_one = euler_d_one_by_one * attitude.D2R
-
-    result = get_result(np.allclose(euler_d_once, euler_d_one_by_one))
-    print('***euler between once and one by one: %s***'% result)
-    print('euler once:\n%s (%s)'% (euler_d_once, euler_r_once))
-    print('euler one by one:\n%s (%s)\n'% (euler_d_one_by_one, euler_r_one_by_one))
-
-'''
 to rotate by 4 ways:
     Rotation
     dcm by scipy.Rotation
@@ -213,153 +184,12 @@ def vectors_rotation(vectors_input, euler_d_input, rot_seq, times = 1, is_extrin
 
     return vectors_rotated_scipy
 
-# the key is the rotvec in space has no change. Just to get rotvec in the new frame
-def euler_in_new_frame(euler_d_old_frame, euler_frame_old_2_new, rot_seq):
-    print('~~~~~~~~~~euler in new frame~~~~~~~~~~')
-
-    rot_frame_old_2_new = Rotation.from_euler(rot_seq, euler_frame_old_2_new, True)
-    euler_frame_old_2_new = rot_frame_old_2_new.as_euler(rot_seq, True) # to get normalized eulers
-    print('***frame from old to new:***')
-    print('euler: %s\n'% euler_frame_old_2_new)
-
-    rot_old_frame = Rotation.from_euler(rot_seq, euler_d_old_frame, True)
-    euler_d_old_frame = rot_old_frame.as_euler(rot_seq, True) # to get normalized eulers
-    rotvec_old_frame = rot_old_frame.as_rotvec()
-    print('***rotation in old frame***:')
-    print('euler:  %s'% euler_d_old_frame)
-    print('rotvec: %s\n'% rotvec_old_frame)
-
-    rotvec_new_frame = rot_frame_old_2_new.inv().apply(rotvec_old_frame)
-    rot_new_frame = Rotation.from_rotvec(rotvec_new_frame)
-    euler_d_new_frame = rot_new_frame.as_euler(rot_seq, True)
-    print('***rotation in new frame***:')
-    print('euler:  %s'% euler_d_new_frame)
-    print('rotvec: %s\n'% rotvec_new_frame)
-
-    return euler_d_new_frame
-
-EULER_D_FRAME_NED_2_ENU = np.array([-90, 180, 0])
-
-def att_ned_2_enu(att_d_ned_2_frd, expected_att_d_enu_2_rfu):
-    att_d_enu_2_rfu = euler_in_new_frame(att_d_ned_2_frd, EULER_D_FRAME_NED_2_ENU, 'ZYX')
-    result = get_result(np.allclose(att_d_enu_2_rfu, expected_att_d_enu_2_rfu))
-    print('***attitude from ned to enu: %s***'% result)
-    print('body from ned to frd:\n%s'% att_d_ned_2_frd)
-    print('body from enu to rfu:\n%s\n'% att_d_enu_2_rfu)
-
-'''
-    rotation from enu (navigation frame) to rfu (body frame), enu / rfu is xyz of coordinates
-    heading vector starts from direction along with north and ends to direction along with its front orient
-    delta_x, delta_y, delta_z indicate tangent direction, which is end direction of heading
-    roll_y indicates self rotation of the heading vector
-'''
-'''
-    way 1: by euler
-'''
-def att_enu_2_rfu_by_euler(delta_x, delta_y, delta_z, roll_y):
-    print('~~~~~~~~~~att from enu to rfu by delta xyz and roll, way 1: euler~~~~~~~~~~')
-
-    yaw_z = math.atan2(-delta_x, delta_y)
-    pitch_x = math.atan2(delta_z, math.sqrt(delta_x * delta_x + delta_y * delta_y))
-
-    euler_r_delta_xyz = np.array([yaw_z, pitch_x, roll_y])
-    euler_d_delta_xyz = euler_r_delta_xyz * attitude.R2D
-    print('euler calculated by delta xyz by zxy sequence intrinsicly:')
-    print('%s (%s)'% (euler_d_delta_xyz, euler_r_delta_xyz))
-
-    rot = Rotation.from_euler("ZXY", euler_r_delta_xyz)
-
-    euler_r_rotated = rot.as_euler('ZYX')
-    euler_d_rotated = euler_r_rotated * attitude.R2D
-    print('euler rotated by zyx sequence:')
-    print('%s (%s)\n'% (euler_d_rotated, euler_r_rotated))
-
-    # test by heading vector
-    heading_start = np.array([0, 1, 0])
-    heading_end = np.array([delta_x, delta_y, delta_z])
-    heading_end = heading_end / np.linalg.norm(heading_end)
-
-    heading_end_rotated = rot.apply(heading_start)
-    result = get_result(np.allclose(heading_end, heading_end_rotated))
-    print('***heading rotated by euler: %s***'% result)
-    print('end heading expected: %s'% heading_end)
-    print('end heading rotated: %s\n'% heading_end_rotated)
-
-def get_vertical_rot_vec(v1, v2):
-    rot_vec = np.cross(v1, v2)
-    rot_angle = np.arccos(np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2)))
-    rot_vec = rot_vec / np.linalg.norm(rot_vec) * rot_angle
-    return rot_vec
-
-'''
-    way 2: by vertical rot vec
-    [ToDo] how to use roll_y
-'''
-def att_enu_2_rfu_by_vertical_rot_vec(delta_x, delta_y, delta_z, roll_y):
-    print('~~~~~~~~~~att from enu to rfu by delta xyz and roll, way 2: by vertical rot vec~~~~~~~~~~')
-
-    heading_start = np.array([0, 1, 0])
-    heading_end = np.array([delta_x, delta_y, delta_z])
-    heading_end = heading_end / np.linalg.norm(heading_end)
-
-    vertical_rot_vec = get_vertical_rot_vec(heading_start, heading_end)
-
-    rot = Rotation.from_rotvec(vertical_rot_vec)
-    euler_r_roated = rot.as_euler('ZYX')
-    euler_d_rotated = euler_r_roated * attitude.R2D
-    print('euler rotated by zyx sequence:')
-    print('%s (%s)\n'% (euler_d_rotated, euler_r_roated))
-
-    heading_end_rotated = rot.apply(heading_start)
-    result = get_result(np.allclose(heading_end, heading_end_rotated))
-    print('***heading rotated by vertical vec: %s***'% result)
-    print('end heading expected: %s'% heading_end)
-    print('end heading rotated: %s\n'% heading_end_rotated)
-
-def delta_att(att_d_input_1, att_d_input_2, rot_seq):
-    print('~~~~~~~~~~delta att~~~~~~~~~~')
-
-    att_r_input_1 = att_d_input_1 * attitude.D2R
-    att_r_input_2 = att_d_input_2 * attitude.D2R
-    print('att input by %s sequence:'% rot_seq)
-    print('%s (%s)'% (att_d_input_1, att_r_input_1))
-    print('%s (%s)\n'% (att_d_input_2, att_r_input_2))
-
-    rot1 = Rotation.from_euler(rot_seq, att_r_input_1)
-    rot2 = Rotation.from_euler(rot_seq, att_r_input_2)
-
-    delta_rot = rot1.inv() * rot2
-
-    delta_att_d = delta_rot.as_euler(rot_seq, True)
-    delta_att_r = delta_rot.as_euler(rot_seq)
-    print('delta att by %s sequence :'% rot_seq)
-    print('%s (%s)'% (delta_att_d, delta_att_r))
-
-    #euler_in_new_frame(delta_rot.as_euler(rot_seq, True), att_d_input_1, rot_seq)
-
-    # test
-    rot2_cal = rot1 * delta_rot
-    att_d_2_cal = rot2_cal.as_euler(rot_seq, True)
-    att_r_2_cal = rot2_cal.as_euler(rot_seq)
-    print('att2 calculated by %s sequence :'% rot_seq)
-    print('%s (%s)'% (att_d_2_cal, att_r_2_cal))
-    result = get_result(np.allclose(att_d_2_cal, att_d_input_2))
-    print('***att_d_input_2 vs att_d_cal_2: %s***\n'% result)
-
 def test_euler_2_x():
     print('============================test euler2x============================')
 
     euler_d_input = np.array([20, 1, 5])
     for seq in ROTATION_SEQUENCES:
         euler_2_x(euler_d_input, seq)
-        break # remove it to test all rotation sequences
-
-def test_euler_one_by_one():
-    print('============================test euler one by one============================')
-
-    euler_d_input = np.array([20, 10, 15])
-    for seq in ROTATION_SEQUENCES:
-        euler_one_by_one(euler_d_input, seq)
         break # remove it to test all rotation sequences
 
 def test_vectors_rotation_once(is_extrinsic):
@@ -414,47 +244,15 @@ def test_vectors_rotation_multple_times(is_extrinsic):
     print('one by one:\n%s'% vectors_rotated_one_by_one)
     print('multiple angles:\n%s\n'% vectors_rotated_multiple_angles)
 
-def test_att_ned_2_enu():
-    att_ned_2_enu(np.array([45, 0, 0]), np.array([-45, 0, 0]))
-    att_ned_2_enu(np.array([0, 45, 0]), np.array([0, 0, 45]))
-    att_ned_2_enu(np.array([0, 0, 45]), np.array([0, 45, 0]))
-    att_ned_2_enu(np.array([90, 45, 90]), np.array([0, 45, 90]))
-
-def test_att_enu_2_rfu_by_delta_xyz():
-    print('============================test att from enu to rfu by delta xyz and roll============================')
-
-    delta_x = -1
-    delta_y = 1
-    delta_z = 0.01#math.sqrt(delta_x * delta_x + delta_y * delta_y)
-    roll_y = 0.1
-    print('delta_x: %s delta_y: %s delta_z: %s roll_y: %s\n'% (delta_x, delta_y, delta_z, roll_y))
-    att_enu_2_rfu_by_euler(delta_x, delta_y, delta_z, roll_y)
-    att_enu_2_rfu_by_vertical_rot_vec(delta_x, delta_y, delta_z, roll_y)
-
-def test_delta_att():
-    print('============================test delta att============================')
-
-    att_d_input_1 = np.array([10, 1, 4])
-    att_d_input_2 = np.array([11, 10, 5])
-
-    delta_att(att_d_input_1, att_d_input_2, 'ZYX')
-    delta_att(att_d_input_1, att_d_input_2, 'zyx')
-
 if __name__ == '__main__':
     np.set_printoptions(precision = 8, suppress = True)
 
     test_euler_2_x()
-    test_euler_one_by_one()
 
     test_vectors_rotation_once(False)
     test_vectors_rotation_multple_times(False)
 
     test_vectors_rotation_once(True)
     test_vectors_rotation_multple_times(True)
-
-    test_att_ned_2_enu()
-
-    test_att_enu_2_rfu_by_delta_xyz()
-    test_delta_att()
 
     print('\n%s: %s\n%s: %s'% (PASSED_STR, PASSED_COUNT, FAILED_STR, FAILED_COUNT))
